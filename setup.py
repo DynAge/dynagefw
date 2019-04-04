3798e7e525d24971f5d90b9495bd5fbc2151b5c0
fliem <franz.liem@gmail.com>
2019-04-04 14:07:05 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+
+descr = """XXX"""
+
+import os
+from setuptools import setup, find_packages
+import glob
+
+DISTNAME = "dynagefw"
+DESCRIPTION = descr
+MAINTAINER = 'Franz Liem'
+MAINTAINER_EMAIL = 'franziskus.liem@uzh.ch'
+LICENSE = 'Apache2.0'
+DOWNLOAD_URL = 'xxx'
+VERSION = "xxx"
+
+PACKAGES = find_packages()
+
+
+
+if __name__ == "__main__":
+
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    import sys
+
+
+    setup(name=DISTNAME,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          license=LICENSE,
+          version=VERSION,
+          url=DOWNLOAD_URL,
+          download_url=DOWNLOAD_URL,
+          packages=PACKAGES,
+          scripts=["scripts/upload_tabular_data.py"],
+          )
