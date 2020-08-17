7dbd1570b123f523cc75e010886b8d800133a298
fliem <franz.liem@gmail.com>
2020-08-17 11:30:17 +0200

    ud download to work with ses

--- dynagefw/gears.py
+++ dynagefw/gears.py
@@ -7,10 +7,13 @@ import pickle
 import os
 from zipfile import ZipFile
 import shutil
+from warnings import warn
 
 
-def run_gear_on_subjects(group_id, project_label, gear, save_dir="~/fw_jobs", config=None, gear_version=None,
-                         analysis_label_suffix="default", api_key=None):
+def run_gear(group_id, project_label, gear, save_dir="~/fw_jobs", config=None, gear_version=None,
+             analysis_label_suffix="default", api_key=None, level="subject"):
+    assert level in ["subject", "session"], f'level needs to be "subject" or "session", not {level}'
+
     api_key = get_fw_api(api_key)
     fw = flywheel.Client(api_key)
     project = fw.lookup(f"{group_id}/{project_label}")
@@ -29,9 +32,14 @@ def run_gear_on_subjects(group_id, project_label, gear, save_dir="~/fw_jobs", co
             else:
                 raise RuntimeError(f"Key {k} not part of config.\n\n{gear.get_default_config()}")
 
-    subjects = []
+    containers = []
     for subject in project.subjects():
-        subjects.append(fw.get_subject(subject.id))
+        if level == "session":
+            for session in subject.sessions():
+                containers.append(session)
+        else:
+            # containers.append(fw.get_subject(subject.id))
+            containers.append(subject)
 
     if analysis_label_suffix:
         analysis_label_suffix = "__" + analysis_label_suffix
@@ -40,16 +48,17 @@ def run_gear_on_subjects(group_id, project_label, gear, save_dir="~/fw_jobs", co
     print(f"project: {project_label}")
     print(f"{gear.gear.name}:{gear.gear.version}")
     print(f"analysis label: {analysis_label}")
+    print(f"level {level}")
 
-    print(f"On {len(subjects)} subjects")
+    print(f"On {len(containers)} {level}s")
     print(f"\n\nConfig:\n")
     pprint(gear_config)
 
     c = input("\n\nContinue (y): ")
     if c == "y":
         analysis_ids = []
-        for subject in subjects:
-            analysis_id = gear.run(analysis_label=analysis_label, config=gear_config, inputs={}, destination=subject)
+        for container in containers:
+            analysis_id = gear.run(analysis_label=analysis_label, config=gear_config, inputs={}, destination=container)
             analysis_ids.append(analysis_id)
 
         save_dir = Path(save_dir).expanduser()
@@ -80,6 +89,9 @@ def check_jobs(analysis_id_file, api_key=None):
     print("JOB INFO\n")
     for k, v in info.items():
         print(f"{k}:\t{len(v)}")
+    if info["failed"]:
+        print("FAILED JOBS:")
+        print(info["failed"])
 
 
 def cancle_jobs(pending=True, running=True, api_key=None):
@@ -115,6 +127,13 @@ def delete_canceled_analysis(group_id, project_label, api_key=None):
 
 
 def download_analysis(group_id, project_label, analysis_label, file_starts_with, save_dir, api_key=None):
+    """
+    Looks for analysis matching the {analysis_label}
+    if they cannot be found on the subject levle, the sessions are queried
+    inside the analysis containers, looks for files that have names that start with {file_starts_with} [if None
+    downloads all]
+    downloads to save_dir, extracts and cleans up tree
+    """
     api_key = get_fw_api(api_key)
     fw = flywheel.Client(api_key)
     project = fw.lookup(f"{group_id}/{project_label}")
@@ -131,26 +150,36 @@ def download_analysis(group_id, project_label, analysis_label, file_starts_with,
     print("DOWNLOAD")
     for subject in project.subjects():
         print(subject.label)
-        analysis = fw.get_container_analyses(subject.id, filter=f'label="{analysis_label}"')
-        assert len(analysis) <= 1, "More than one analysis found"
-        if analysis:
-            if analysis[0].files:
-                files = [file_obj.name for file_obj in analysis[0].files]
-            else:
-                files = []
+        analyses = fw.get_container_analyses(subject.id, filter=f'label="{analysis_label}"')
+        assert len(analyses) <= 1, "More than one analysis found"
 
-            # filter files
-            download_files = []
-            if file_starts_with:
-                for f in files:
-                    if f.startswith(file_starts_with):
-                        download_files.append(f)
-            else:
-                download_files = files
+        # if no analyses on subject level, look into sessions
+        if not analyses:
+            analyses = []
+            for session in subject.sessions():
+                analyses_session = fw.get_container_analyses(session.id, filter=f'label="{analysis_label}"')
+                assert len(analyses_session) <= 1, "More than one analysis found"
+                analyses += analyses_session
 
-            for file in download_files:
-                print(file)
-                analysis[0].download_file(file, file)
+        if analyses:
+            for analysis in analyses:
+                if analysis.files:
+                    files = [file_obj.name for file_obj in analysis.files]
+                else:
+                    files = []
+
+                # filter files
+                download_files = []
+                if file_starts_with:
+                    for f in files:
+                        if f.startswith(file_starts_with):
+                            download_files.append(f)
+                else:
+                    download_files = files
+
+                for file in download_files:
+                    print(file)
+                    analysis.download_file(file, file)
 
     print("UNZIP")
     zipfiles = list(zip_out_dir.glob("*.zip"))
@@ -159,8 +188,23 @@ def download_analysis(group_id, project_label, analysis_label, file_starts_with,
             zip_ref.extractall(extract_out_dir)
 
     print("MOVE AND CLEAN UP")
+    # remove id hierarchy and move to out_dir
+    # e.g., '5f34/sub-x/ses-y/func/*bold.json'
+    #       'sub-x/ses-y/func/*bold.json'
     os.chdir(extract_out_dir)
-    os.system(f"mv */* {out_dir}")
+    file_tree = [f for f in Path(".").rglob("*") if f.is_file()]
+    file_overwritten = []
+    for in_file in file_tree:
+        out_file = out_dir / Path(*in_file.parts[1:])
+        out_file.parent.mkdir(parents=True, exist_ok=True)
+        print(in_file, out_file)
+        if out_file.is_file():
+            file_overwritten.append(out_file)
+        in_file.rename(out_file)
+
+    if file_overwritten:
+        warn(f"{len(set(file_overwritten))} have been overwritten, because of multiple files with the same name in "
+             f"different analyses.\n{set(file_overwritten)}")
     shutil.rmtree(extract_out_dir)
     shutil.rmtree(zip_out_dir)
 
